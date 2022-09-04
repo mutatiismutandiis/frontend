8cfe468975324114266efc4da6774ac1490986dd
Pau Cardone <96506186+mutatiismutandiis@users.noreply.github.com>
2022-09-04 20:25:07 +0200

    frontend terminado

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UpperbarComponent } from './components/upperbar/upperbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { PersonComponent } from './components/person/person.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UpperbarComponent,
+    BannerComponent,
+    PersonComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
